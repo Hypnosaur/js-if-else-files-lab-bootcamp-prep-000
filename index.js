b0500f792d7042647dee03993a55d89ff520e483
Laura Fretter <fretterlaura@gmail.com>
2019-02-25 20:53:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var height = "74"
 if (name === "Susan"){
   console.log("The name is correct");
 } else {
-  consolelog("expected name to be Susan")
+  console.log("expected name to be Susan")
 }
 // Don't worry about this
 module.exports = { name, height
